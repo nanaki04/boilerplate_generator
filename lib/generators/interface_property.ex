b30b0f3edf80b981150fbb68f8ef15fa122f0d3c
zwetsloot robert jan <zwetsloot.robert.jan@techcross.co.jp>
2018-03-02 09:33:57 +0900

    and added support for interface property accessibility generation

--- lib/generators/interface_property.ex
+++ lib/generators/interface_property.ex
@@ -7,6 +7,7 @@ defmodule BoilerplateGenerator.InterfaceProperty do
   @spec generate(state, property) :: String.t
   def generate(state, property) do
     state.interface_property_template
+    |> String.replace(~r/<\{accessibility\}>/, Property.accessibility property)
     |> String.replace(~r/<\{description\}>/, Property.description property)
     |> String.replace(~r/<\{type\}>/, Property.type property)
     |> String.replace(~r/<\{name\}>/, Property.name property)
